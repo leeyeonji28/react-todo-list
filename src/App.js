c6608ee3c71c2761708b12ea529ab6a909ee1ef3
leeyeonji28 <yeon102800@gmail.com>
2022-08-30 22:44:28 +0900

    할 일 리스트 출력 백엔드 연동

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import { useRef, useState } from "react";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      content: "기상",
-      checked: false,
-    },
-    {
-      id: 2,
-      content: "씻기",
-      checked: false,
-    },
-    {
-      id: 3,
-      content: "밥먹기",
-      checked: false,
-    },
-  ]);
-  const nextId = useRef(4); //변경되도 렌더링을 안 함
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState("");
+  useEffect(() => {
+    const getList = async () => {
+      const json = await axios({
+        url: "http://localhost:8090/todos",
+        method: "GET",
+      });
+      setTodos(json.data);
+      // console.log(json.data);
+    };
+
+    getList();
+  }, []);
+
   // let getList = async () => {
   //   try {
   //     const json = await axios({
@@ -40,6 +37,8 @@ function App() {
   // useEffect(() => {
   //   getList();
   // }, []);
+
+  const nextId = useRef(4); //변경되도 렌더링을 안 함
 
   return (
     <div className="max-w-4xl m-auto">
