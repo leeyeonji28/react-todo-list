be2c833123bd69a5bc182908751347a56c7e6dad
leeyeonji28 <yeon102800@gmail.com>
2022-09-04 23:52:07 +0900

    할 일 수정 API 연결 및 UI 수정

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,6 +8,7 @@ const TodoList = ({ todos, setTodos }) => {
         <table className="table w-full">
           <thead>
             <tr>
+              <td></td>
               <th>ID</th>
               <th>할 일</th>
               <th>완료 상태</th>
@@ -26,30 +27,6 @@ const TodoList = ({ todos, setTodos }) => {
             ))}
           </tbody>
         </table>
-
-        <input type="checkbox" id="my-modal-5" className="modal-toggle" />
-        <div className="modal">
-          <div className="modal-box w-11/12 max-w-3xl">
-            <form
-              onSubmit={(e) => {
-                // e.preventDefault();
-                alert("제출");
-              }}
-            >
-              <h3 className="mb-5 text-lg">수정할 내용을 입력해 주세요.</h3>
-              <input
-                type="text"
-                placeholder="수정할 내용를 입력해 주세요."
-                className="input input-bordered w-full"
-              />
-            </form>
-            <div className="modal-action">
-              <label for="my-modal-5" className="btn">
-                X
-              </label>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
 import { FiEdit3, FiTrash2 } from "react-icons/fi";
 import axios from "axios";
+import { useState } from "react";
 
 const TodoListItem = ({ todo, setTodos, index }) => {
   const { id, checked, content } = todo;
+  const [showModal, setShowModal] = useState(false);
+  const [modicon, setModicon] = useState(content);
   return (
     <tr>
+      <td></td>
       <th>{index + 1}</th>
       {checked ? (
         <td className="line-through text-gray-400">{content}</td>
@@ -30,9 +34,59 @@ const TodoListItem = ({ todo, setTodos, index }) => {
       </td>
       <td className="edit cursor-pointer">
         <div for="my-modal-5">
-          <label for="my-modal-5" class="btn btn-active btn-ghost">
+          <label
+            class="btn btn-active btn-ghost"
+            onClick={() => {
+              setShowModal(!showModal);
+            }}
+          >
             <FiEdit3 />
           </label>
+          <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+          {showModal == true ? (
+            <div className="w-full h-full fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/[.2] z-100">
+              <div className="modal-box w-120 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <form
+                  onSubmit={async (e) => {
+                    e.preventDefault();
+                    alert("할 일 수정이 완료되었습니다.");
+                    const data = await axios({
+                      url: `http://localhost:8090/todos/edit/${id}`,
+                      method: "PATCH",
+                      data: { content: modicon },
+                    });
+                    setTodos(data.data);
+                    setShowModal(false);
+                  }}
+                >
+                  <h3 className="mb-5 text-lg font-bold">
+                    수정할 내용을 입력해 주세요.
+                  </h3>
+                  <input
+                    type="text"
+                    value={modicon}
+                    className="input input-bordered w-full"
+                    onChange={(e) => {
+                      setModicon(e.target.value);
+                    }}
+                  />
+                </form>
+                <div className="modal-action">
+                  <label
+                    for="my-modal-5"
+                    className="btn"
+                    onClick={() => {
+                      setShowModal(false);
+                    }}
+                  >
+                    X
+                  </label>
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </td>
       <td className="remove cursor-pointer">
